fd479669e29c7affa3ed5537b56363730dbab8af
Jack <99360106+Jackknife95@users.noreply.github.com>
2024-04-14 15:09:14 +0200

    abstracción en registraPelicula + añadir plantilla y funciones nuevas en clase Pelicula

--- p3_g10/includes/Pelicula.php
+++ p3_g10/includes/Pelicula.php
@@ -7,6 +7,62 @@ class Pelicula{
     private $sinopsis;
     private $foto;
 
+    public static function buscaPelicula($titulo){
+
+        if (!$titulo) {
+            return false;
+        } 
+        
+        $conn = Aplicacion::getInstance()->getConexionBd();
+
+        $t= $conn->real_escape_string($titulo);
+
+        $query = "SELECT titulo FROM peliculas WHERE titulo = '$titulo' ";
+
+        $result = $conn->query($query);
+
+        if ($result->num_rows == 1){
+            return $row= $result->fetch_array();
+        }
+
+        return false;
+    }
+
+    public static function insertaPelicula($titulo,$descripcion,$alt,$fecha_estreno,$archivo,$temp,$genero){
+
+        if (!$titulo||!$descripcion||!$alt||!$fecha_estreno) {
+            return false;
+        } 
+        $conn = Aplicacion::getInstance()->getConexionBd();
+
+        if(!self::buscaPelicula($titulo)){
+            $t= $conn->real_escape_string($titulo);
+            $d=$conn->real_escape_string($descripcion);
+            $a=$conn->real_escape_string($alt);
+            $f=$conn->real_escape_string($fecha_estreno);
+            $g=$conn->real_escape_string($genero);
+
+
+            $dir='./images/'.date("Y-m-d-H-i-s-").$archivo;
+            if (move_uploaded_file($temp, $dir)) {
+                
+                chmod($dir, 0777);
+                $query = "INSERT INTO peliculas (titulo, descripcion, fecha_estreno,direccion_fotografia,alt) VALUES ('$titulo', '$descripcion', '$fecha_estreno','$dir','$alt')";
+            
+                $result = $conn->query($query);
+
+                $query = "INSERT INTO genero_peliculas (titulo, genero) VALUES ('$t', '$genero')";
+            
+                $result = $conn->query($query);
+
+
+                return true;
+        }
+        return false;
+
+        }
+    }
+
     public static function borrarPelicula($titulo)
     {
         if (!$titulo) {
