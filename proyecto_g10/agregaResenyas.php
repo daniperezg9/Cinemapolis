4b74e4959e4ac14b4b6b9c10d37cfdb3d61cda75
SirmaSosa <sirsosa@ucm.es>
2024-05-06 11:10:57 +0200

    Update agregaResenyas.php

--- proyecto_g10/agregaResenyas.php
+++ proyecto_g10/agregaResenyas.php
@@ -1,16 +1,12 @@
 <?php
 namespace cinemapolis;
 require_once __DIR__.'/includes/config.php';
-//Definicion de constantes
-//Parametros de acceso de la base de datos
-    $resenya =$_POST['resenya'];
-    $pelicula =$_POST['pelicula'];
-    $contacto=$_SESSION['contacto'];
 
-    Resenyas::añadirResenya($pelicula,$contacto,$resenya);
-
-    $url_destino = './resenyasYvaloraciones.php?titulo=' . $_POST['pelicula'];
-    header('Location:' . $url_destino);
+    $formReg = new FormularioAnyadirResenya();
+    $htmlForm=$formReg->gestiona();
+    $contenidoPrincipal=<<<EOS
+        $htmlForm
+    EOS;
     
 require __DIR__.'/includes/vistas/plantillas/plantilla.php';
 ?>
