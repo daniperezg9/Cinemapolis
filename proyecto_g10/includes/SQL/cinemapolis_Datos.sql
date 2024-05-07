46b7b67673e8e5d34f8302edf20ee29ef72acad8
Daniel Pérez García <danper09@ucm.es>
2024-05-08 00:26:38 +0200

    Añadidos datos a la BD

--- proyecto_g10/includes/SQL/cinemapolis_Datos.sql
+++ proyecto_g10/includes/SQL/cinemapolis_Datos.sql
@@ -33,7 +33,8 @@ TRUNCATE TABLE `eventos`;
 
 INSERT INTO `eventos` (`creador_evento`, `nombre_evento`, `descripcion_evento`, `fecha_evento`, `fecha_creacion`) VALUES
 ('admin@admin.es', 'Quedada para hablar del valle de las sombras', 'Os espero en el Meet', '2024-05-12', '2024-05-07'),
-('admin@admin.es', 'Veamos Nimona', 'Vamos a ver nimona a las 5PM', '2024-05-10', '2024-05-07');
+('admin@admin.es', 'Veamos Nimona', 'Vamos a ver nimona a las 5PM', '2024-05-10', '2024-05-07'),
+('user4@gmail.com', 'Noche de pelis retro', 'Te invito a ver los mejores clásicos del cine', '2024-05-11', '2024-05-08');
 
 --
 -- Truncar tablas antes de insertar `foro`
@@ -66,7 +67,9 @@ INSERT INTO `genero_peliculas` (`titulo`, `genero`) VALUES
 ('Misi&oacute;n hostil', 'Acci&oacute;n'),
 ('Nimona', 'Animaci&oacute;n'),
 ('Tarot', 'Terror'),
-('Valle de sombras', 'Ciencia ficci&oacute;n');
+('Valle de sombras', 'Ciencia ficci&oacute;n'),
+('Mary Poppins', 'Fantasía'),
+('El bueno, el feo y el malo', 'Western');
 
 --
 -- Truncar tablas antes de insertar `lista_foros`
@@ -94,11 +97,13 @@ TRUNCATE TABLE `peliculas`;
 --
 
 INSERT INTO `peliculas` (`titulo`, `descripcion`, `fecha_estreno`, `direccion_fotografia`, `alt`) VALUES
-('Garfield: la pelicula', 'El mundialmente famoso Garfield, el gato casero que odia los lunes y que adora la lasa&ntilde;a, est&aacute; a punto de vivir una aventura &iexcl;en el salvaje mundo exterior! Tras una inesperada reuni&oacute;n con su largamente perdido padre &ndash;el desali&ntilde;ado gato callejero Vic&ndash; Garfield y su amigo canino Odie se ven forzados a abandonar sus perfectas y consentidas vidas al unirse a Vic en un hilarante y muy arriesgado atraco.', '2024-05-01', './images/2024-05-07-20-20-30-nimona.jpg', 'gato naranja sobre un sofa'),
+('Garfield: la pelicula', 'El mundialmente famoso Garfield, el gato casero que odia los lunes y que adora la lasa&ntilde;a, est&aacute; a punto de vivir una aventura &iexcl;en el salvaje mundo exterior! Tras una inesperada reuni&oacute;n con su largamente perdido padre &ndash;el desali&ntilde;ado gato callejero Vic&ndash; Garfield y su amigo canino Odie se ven forzados a abandonar sus perfectas y consentidas vidas al unirse a Vic en un hilarante y muy arriesgado atraco.', '2024-05-01', './images/2024-05-07-20-20-30-garfield.jpg', 'gato naranja sobre un sofa'),
 ('Misi&oacute;n hostil', 'Reaper es un piloto de drones de la Fuerza A&eacute;rea que apoya una misi&oacute;n especial en el sur de Filipinas. Tras ver movimiento en la jungla durante su reconocimiento a&eacute;reo, la situaci&oacute;n toma un vuelco inesperado. El equipo de tierra, en el que se encuentran el sargento Kinney y sus compa&ntilde;eros, es atacado de manera fulminante y capturado por un grupo de insurgentes.', '2024-05-01', './images/2024-05-07-20-41-45-Misión hostil.jpg', 'un hombre con un arma de fuego'),
 ('Nimona', 'En un mundo medieval futurista, el caballero Ballister Bravocoraz&oacute;n es acusado de un crimen que no ha cometido, y la &uacute;nica persona que puede ayudarlo a demostrar su inocencia es Nimona, una traviesa adolescente con inclinaci&oacute;n por el caos&hellip; que adem&aacute;s resulta ser el ser metamorfo al que debe destruir', '2023-06-18', './images/2024-05-07-20-23-40-nimona.jpg', 'ni&ntilde;a con el pelo rosa y alas de demonio'),
 ('Tarot', 'Cuando un grupo de amigos infringe de manera imprudente la regla sagrada de la lectura de las cartas del Tarot -nunca se debe utilizar la baraja de otra persona-, desatan sin saberlo un mal atrapado en las cartas malditas. Uno a uno, se enfrentan cara a cara al destino y acaban en una carrera contra la muerte para escapar del futuro que las cartas predicen.', '2024-05-10', './images/2024-05-07-20-26-23-Tarot.jpg', 'Una mano con una carta de tarot'),
-('Valle de sombras', 'Cordillera del Himalaya, a&ntilde;o 1999. Quique, Clara y el peque&ntilde;o Lucas disfrutan de sus primeras vacaciones juntos en el norte de la India. Una noche, durmiendo al raso durante una tormenta, sufren un brutal ataque por unos bandidos. Horas despu&eacute;s, Quique es rescatado por un nativo y trasladado a una remota aldea aislada en las monta&ntilde;as', '2024-05-03', './images/2024-05-07-20-48-10-valle_de_sombras.jpg', 'Un hombre mirando a la nada');
+('Valle de sombras', 'Cordillera del Himalaya, a&ntilde;o 1999. Quique, Clara y el peque&ntilde;o Lucas disfrutan de sus primeras vacaciones juntos en el norte de la India. Una noche, durmiendo al raso durante una tormenta, sufren un brutal ataque por unos bandidos. Horas despu&eacute;s, Quique es rescatado por un nativo y trasladado a una remota aldea aislada en las monta&ntilde;as', '2024-05-03', './images/2024-05-07-20-48-10-valle_de_sombras.jpg', 'Un hombre mirando a la nada'),
+('Mary Poppins', 'La vida de dos niños rebeldes que pretenden llamar la atención de sus padres haciendo la vida imposible a todas las niñeras, se verá alterada con la llegada de Mary Poppins, una institutriz que baja de las nubes usando su paraguas como paracaídas.', '1965-12-02', './images/mary-poppins-portada.jpg', 'Mary Poppins volando'),
+('El bueno, el feo y el malo', 'Los protagonistas son tres cazadores de recompensas que buscan un tesoro que ninguno de ellos puede encontrar sin la ayuda de los otros dos. Así que los tres colaboran entre sí, al menos en apariencia.', '1968-08-07', './images/bueno-feo-y-malo.jpg', 'El bueno, el feo y el malo de pie');
 
 --
 -- Truncar tablas antes de insertar `resenyas`
@@ -149,7 +154,9 @@ INSERT INTO `valoraciones` (`contacto`, `pelicula`, `puntuacion`) VALUES
 ('admin@admin.es', 'Nimona', 10),
 ('admin@admin.es', 'Tarot', 7),
 ('admin@admin.es', 'Valle de sombras', 3),
-('user1@gmail.com', 'Tarot', 10);
+('user1@gmail.com', 'Tarot', 10),
+('user2@gmail.com', 'El bueno, el feo y el malo', 10),
+('user5@gmail.com', 'Mary Poppins', 9);
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
