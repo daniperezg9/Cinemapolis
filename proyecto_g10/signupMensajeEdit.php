ce155d9be0608c7894e8549e263ff4dc6a62979a
Jack <99360106+Jackknife95@users.noreply.github.com>
2024-05-08 18:43:18 +0200

    limpieza de archivos y código

--- proyecto_g10/signupMensajeEdit.php
+++ proyecto_g10/signupMensajeEdit.php
@@ -11,15 +11,4 @@ $contenidoPrincipal = <<<EOS
     $htmlForm
 EOS;
 
-/*$contenidoPrincipal = <<<EOS
-    <form action =  "./editaMensaje.php"  method = "post">
-    <fieldset>
-        <legend>Edita Mensaje</legend>
-        <input type="hidden" name="fecha_envio" value="$_POST[fecha_envio]">
-        Mensaje:<br> <input type="text" name="mensaje"><br>
-        <input type="submit" name="Enviar">
-    </fieldset>
-    </form>    
-EOS;*/
-
 require __DIR__.'/includes/vistas/plantillas/plantilla.php';
